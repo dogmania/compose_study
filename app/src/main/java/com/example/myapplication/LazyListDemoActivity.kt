79bb9c005092d56e475a8285fe9f5fbee5555c2a
dogmania <hj296914@gmail.com>
2024-03-30 23:47:18 +0900

    LazyList의 스티키 헤더와 스크롤 버튼 학습

--- app/src/main/java/com/example/myapplication/LazyListDemoActivity.kt
+++ app/src/main/java/com/example/myapplication/LazyListDemoActivity.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,15 +21,20 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,6 +42,7 @@ import androidx.compose.ui.unit.dp
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import kotlinx.coroutines.launch
 
 class LazyListDemo : ComponentActivity() {
     private var itemArray: Array<String>? = null
@@ -53,9 +65,16 @@ class LazyListDemo : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MainScreen(itemArray: Array<out String>) {
     val context = LocalContext.current
+    val groupedItems = itemArray.groupBy { it.substringBefore(' ') }
+
+    val listState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+    val displayButton = listState.firstVisibleItemIndex > 5 // 버튼 표시 여부를 나타내는 bool 변수
+
     val onListItemClick = { text: String ->
         Toast.makeText(
             context,
@@ -64,9 +83,47 @@ fun MainScreen(itemArray: Array<out String>) {
         ).show()
     }
 
-    LazyColumn() {
-        items(itemArray) { model ->
-            MyCardListItem(item = model, onItemClick = onListItemClick)
+    Box {
+        LazyColumn(
+            state = listState,
+            contentPadding = PaddingValues(bottom = 40.dp)
+        ) {
+            groupedItems.forEach { (manufacturer, models) ->
+                stickyHeader {
+                    Text(
+                        text = manufacturer,
+                        color = Color.White,
+                        modifier = Modifier
+                            .background(Color.Gray)
+                            .padding(5.dp)
+                            .fillMaxWidth()
+                    )
+                }
+
+                items(models) { model ->
+                    MyCardListItem(item = model, onItemClick = onListItemClick)
+                }
+            }
+        }
+
+        AnimatedVisibility(visible = displayButton,
+            Modifier.align(Alignment.BottomCenter)
+        ) {
+            OutlinedButton(
+                onClick = {
+                    coroutineScope.launch {
+                        listState.scrollToItem(0)
+                    }
+                },
+                border = BorderStroke(1.dp, Color.Gray),
+                shape = RoundedCornerShape(50),
+                colors = ButtonDefaults.outlinedButtonColors(
+                    contentColor = Color.DarkGray
+                ),
+                modifier = Modifier.padding(5.dp)
+            ) {
+                Text("Top")
+            }
         }
     }
 }
@@ -91,7 +148,7 @@ fun MyCardListItem(item: String, onItemClick: (String) -> (Unit)) {
         modifier = Modifier
             .padding(8.dp)
             .fillMaxWidth()
-            .clickable {onItemClick(item)},
+            .clickable { onItemClick(item) },
         shape = RoundedCornerShape(10.dp),
         elevation = CardDefaults.cardElevation(
             defaultElevation = 5.dp
